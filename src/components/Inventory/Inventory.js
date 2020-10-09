105b09a3ad8ba941b3e1cc593bb735694bd15a98
rafsan <rafsanamin18@gmail.com>
2020-10-10 01:47:31 +0600

    50.6

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -2,18 +2,34 @@ import React from "react";
 
 const Inventory = () => {
     const handleAddProduct = () => {
+        const product = {};
         fetch("http://localhost:5000/addProduct", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({}),
+            body: JSON.stringify(product),
         });
     };
 
     return (
         <div>
-            <button className="btn btn-warning" onClick={handleAddProduct}>
-                Add Product
-            </button>
+            <form action="">
+                <p>
+                    <span>Name:</span>
+                    <input type="name" />
+                </p>
+                <p>
+                    <span>Price:</span>
+                    <input type="price" />
+                </p>
+                <p>
+                    <span>Quantity:</span>
+                    <input type="quantity" />
+                </p>
+                <p>
+                    <span>Product Image</span>
+                    <input type="file" />
+                </p>
+            </form>
         </div>
     );
 };
