827bfc3c556a3d4dde30adc64d490653af202e32
rafsan <rafsanamin18@gmail.com>
2020-10-10 11:06:50 +0600

    53.3

---
+++ src/components/ProcessPayment.js/ProcessPayment.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { CardElement } from "@stripe/react-stripe-js";
+import SimpleCardForm from "./SimpleCardForm";
+
+const ProcessPayment = () => {
+    const stripePromise = loadStripe(
+        "pk_test_51HaTGyDDhGhnZ8yfdnYLR4vlB0auqUsZpJ5IpSxOan0h8e4U38FvZg3e0ngzqOKJcPNRKSnyplipyL9B1Pm5jNPs00aZ5j2wCq"
+    );
+    return (
+        <div>
+            <Elements stripe={stripePromise}>
+                <SimpleCardForm></SimpleCardForm>
+            </Elements>
+        </div>
+    );
+};
+
+export default ProcessPayment;
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { UserContext } from "../../App";
 import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
+import ProcessPayment from "../ProcessPayment.js/ProcessPayment";
 import "./Shipment.css";
 
 const Shipment = () => {
@@ -35,47 +36,58 @@ const Shipment = () => {
     console.log(watch("example")); // watch input value by passing the name of it
 
     return (
-        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-            <input
-                name="name"
-                defaultValue={loggedInUser.name}
-                ref={register({ required: true })}
-                placeholder="Your name"
-            />
+        <div className="row">
+            <div className="col-md-6">
+                <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        name="name"
+                        defaultValue={loggedInUser.name}
+                        ref={register({ required: true })}
+                        placeholder="Your name"
+                    />
 
-            {errors.name && <span className="error">Name is required</span>}
+                    {errors.name && (
+                        <span className="error">Name is required</span>
+                    )}
 
-            <input
-                name="email"
-                defaultValue={loggedInUser.email}
-                ref={register({ required: true })}
-                placeholder="Your email"
-            />
+                    <input
+                        name="email"
+                        defaultValue={loggedInUser.email}
+                        ref={register({ required: true })}
+                        placeholder="Your email"
+                    />
 
-            {errors.email && <span className="error">Email is required</span>}
+                    {errors.email && (
+                        <span className="error">Email is required</span>
+                    )}
 
-            <input
-                name="address"
-                ref={register({ required: true })}
-                placeholder="Your Address"
-            />
+                    <input
+                        name="address"
+                        ref={register({ required: true })}
+                        placeholder="Your Address"
+                    />
 
-            {errors.address && (
-                <span className="error">Address is required</span>
-            )}
+                    {errors.address && (
+                        <span className="error">Address is required</span>
+                    )}
 
-            <input
-                name="phone"
-                ref={register({ required: true })}
-                placeholder="Your Phone Number"
-            />
+                    <input
+                        name="phone"
+                        ref={register({ required: true })}
+                        placeholder="Your Phone Number"
+                    />
 
-            {errors.phone && (
-                <span className="error">Phone number is required</span>
-            )}
+                    {errors.phone && (
+                        <span className="error">Phone number is required</span>
+                    )}
 
-            <input type="submit" />
-        </form>
+                    <input type="submit" />
+                </form>
+            </div>
+            <div className="col-md-6">
+                <ProcessPayment></ProcessPayment>
+            </div>
+        </div>
     );
 };
 
